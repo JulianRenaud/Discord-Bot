683cf9c61bbab45e48ccc341fd3b5a117934d4b2
JulianRenaud <midway6220@gmail.com>
2022-10-21 17:16:04 -0500

    Done with e1

--- Discord-Bot/Bot.cs
+++ Discord-Bot/Bot.cs
@@ -1,6 +1,8 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +17,21 @@ namespace Discord_Bot
         public CommandsNextExtension Commands { get; private set; }
         public async Task RunAsync()
         {
-            var config = new DiscordConfiguration
-            {
+            var json = string.Empty;
 
-            };
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+                var config = new DiscordConfiguration
+                {
+                    Token = configJson.Token,
+                    TokenType = TokenType.Bot,
+                    AutoReconnect = true,
+                    MinimumLogLevel = LogLevel.Debug
+                };
             
             Client = new DiscordClient(config);
 
@@ -26,19 +39,21 @@ namespace Discord_Bot
 
             var commandsConfig = new CommandsNextConfiguration
             {
-
+                StringPrefixes = new String[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true
             };
 
             Commands = Client.UseCommandsNext(commandsConfig);
 
             await Client.ConnectAsync();
 
-            await Task.Delay(1);
+            await Task.Delay(-1);
         }
 
         private Task OnClientReady(DiscordClient client, ReadyEventArgs e)
         {
-            return null;
+            return Task.CompletedTask;
         }
     }
 }
--- Discord-Bot/Program.cs
+++ Discord-Bot/Program.cs
@@ -2,3 +2,4 @@
 using DSharpPlus.Entities;
 
 var bot = new Bot();
+bot.RunAsync().GetAwaiter().GetResult();
